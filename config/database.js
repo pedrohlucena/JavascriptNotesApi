d7ab3231ff17bc0d45230f8505cd698343b6ffbd
Pedro Henrique Lucena Brandão <pedrohlucena413@gmail.com>
2021-10-27 15:37:43 -0300

    new change

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 require('dotenv').config()
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
+console.log(MONGO_URL)
 
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true
-}).then(() => console.log('Connection succesful'))
-  .catch((err) => console.log(err));
+}).then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
